db8700d3ddfa95098dc0c97caca42051369f463b
lucasdossantosdesouza <lucasdossantosdesouza@gmail.com>
2020-10-22 01:16:24 -0400

    validators

---
+++ Curso Angular Spring/FormValidation/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'FormValidation';
+
+  name: string = "";
+  @Input()
+  email: string | boolean;
+
+  onSubmit(f: NgForm) {
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
+  }
+}
