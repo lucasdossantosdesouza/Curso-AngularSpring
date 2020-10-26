20e4194029aa6e941a405cca1fd86a094b150da5
lucasdossantosdesouza <lucasdossantosdesouza@gmail.com>
2020-10-26 16:34:12 -0400

    layout finalizado

--- Curso Angular Spring/HelpDesk/src/app/app.module.ts
+++ Curso Angular Spring/HelpDesk/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MenuComponent } from './component/menu/menu.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent    
+    HeaderComponent,
+    MenuComponent,
+    FooterComponent    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule   
   ],
   providers: [],
   bootstrap: [AppComponent]
