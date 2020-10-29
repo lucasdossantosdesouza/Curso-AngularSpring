04683609c7fb50c0f12a76fd0a43e8cb4342cb3d
lucasdossantosdesouza <lucasdossantosdesouza@gmail.com>
2020-10-29 00:21:16 -0400

    permissões e usuario logado

--- Curso Angular Spring/HelpDesk/src/app/component/header/header.component.ts
+++ Curso Angular Spring/HelpDesk/src/app/component/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Usuario } from './../../model/usuario';
+import { Usuario } from 'src/app/model/usuario';
 import { SharedService } from 'src/app/services/shared.service';
 import { Component, Input, OnInit } from '@angular/core';
 
@@ -9,12 +9,23 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   @Input() title: string;
-   constructor() {
-  
+  shared: SharedService;
+  usuario:Usuario;
+
+  constructor() { 
+    this.shared = SharedService.getInstance();
+    this.usuario = new Usuario();
   }
  
   ngOnInit(): void {
  
+  }
+
+  signOut(): void{
+    this.shared.token = null;
+    this.shared.usuario = null;
+    window.location.href = '/login';
+    window.location.reload();
   }
 
 }
--- Curso Angular Spring/HelpDesk/src/app/component/menu/menu.component.ts
+++ Curso Angular Spring/HelpDesk/src/app/component/menu/menu.component.ts
@@ -1,3 +1,4 @@
+import { SharedService } from 'src/app/services/shared.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
   
-  constructor() { }
+  shared: SharedService;
+  constructor() { 
+    this.shared = SharedService.getInstance();
+  }
 
   ngOnInit(): void {
   }
